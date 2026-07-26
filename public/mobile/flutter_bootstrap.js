67bea89023f9a466b3787d993d984907cac7c844
Soumojit Saha <wbsoumo@Soumojits-MacBook-Air.local>
2026-07-27 02:06:33 +0530

    Deploy rebuilt Flutter web and APK bundles with dynamic server authentication integration

--- public/mobile/flutter_bootstrap.js
+++ public/mobile/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2858386501" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "33532035" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
